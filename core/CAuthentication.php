1942ecae00647f3b097fcbd2dc7485efc37e22f1
xupyprmv <xupypr@xupypr.com>
2011-08-17 00:00:43 +0400

    authentication checks server status and validate credentials

--- core/CAuthentication.php
+++ core/CAuthentication.php
@@ -20,14 +20,27 @@ class CAuthentication {
 	 * @return bool log in success
 	 */
 	public static function login($userId = null, $fullAPIKey = null) {
-		// TODO Check that credential are valid
-		if (!empty($_POST['userId']) && !empty($_POST['APIKey'])) {
-			echo '2';
+		if (!empty($_POST['userId']) && !empty($_POST['APIKey'])) {		
 			$_SESSION['userId'] = $_POST['userId'];
 			$_SESSION['APIKey'] = $_POST['APIKey'];
-			$_SESSION['logged in'] = true;
-			return true;
-		} 
+			
+			// Check that server is on
+			$status = Core::getEVEAPI()->getServerStatus();
+			if (strtolower($status['result']['serverOpen'])!='true') {
+				Core::viewErrorPage('EVE Server is offline');
+			} else {			
+				// Check that credential are valid
+				$characters = Core::getEVEAPI($_SESSION['APIKey'])->getCharacters($_SESSION['userId']);
+				if (!empty($characters['error'])) {
+					// TODO Ban user IP after 3 unsuccessfull tries for 1 hour
+					Core::viewErrorPage($characters['error']);				
+				} else {
+					$_SESSION['logged in'] = true;
+					return true;
+				}
+			}					
+		}
+		CAuthentication::viewLoginPage();	
 		return false;
 	}
 	

9726030ff61595b767659796778313faab6c2ea7
xupyprmv <xupypr@xupypr.com>
2011-08-17 00:20:07 +0400

    correct imports

--- install.php
+++ install.php
@@ -1,4 +1,12 @@
 <?php
-	include('core/Core.php');
-	Core::install();
+function __autoload($class_name) {
+	if (strpos($class_name, "C")===0) {
+		require_once './core/'.$class_name.'.php';
+	} else if (strpos($class_name, "E")===0) {
+		require_once './eveapi/'.$class_name.'.php';	
+	} else if (strpos($class_name, "R")===0) {
+		require_once './reports/'.$class_name.'.php';	
+	}	
+}
+Core::install();
 ?>
